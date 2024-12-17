f8526c4b122cc7ce0b9e10c25d28ea6a1e690259
Sparven0 <gustafwingren@hotmail.se>
2024-12-17 15:08:19 +0100

    Large Update

--- organizer4github/src/contents.js
+++ organizer4github/src/contents.js
@@ -1,6 +1,6 @@
-const taskDescription = document.querySelector('.description');
+/*const taskDescription = document.querySelector('.description');
 taskDescription.innerHTML = 'Task 1 consists of organizing, counting and documenting material e.t.c in -2 North warehouse';
 
 
 const taskCardTitle = document.querySelector('.taskTitle');
-taskCardTitle.innerHTML = 'Task 1'
+taskCardTitle.innerHTML = 'Task 1'*/
--- organizer4github/src/requests.js
+++ organizer4github/src/requests.js
@@ -1,276 +1,168 @@
 import { BASE_URL } from "./api.js";
+let right = document.querySelectorAll(".right");
+const closeBtn = document.querySelectorAll('.close')
+const flipper = document.querySelector('.flipshower')
+const flipCard = document.querySelector('.flip-card');
+const whoops = document.querySelector('.whoops')
 
-const detailsBox = document.querySelectorAll(".detailsBox");
-const detailsQty = document.querySelectorAll(".qtySectionD");
-const detailsSize = document.querySelectorAll(".sizeSectionD");
-const detailsLocation = document.querySelectorAll(".locationSectionD");
-const closeDetails = document.querySelectorAll(".closeDetails");
-const detailsBtn = document.querySelectorAll(".details");
-const detailsNames = document.querySelectorAll(".nameSectionD");
-const qtySecs = document.querySelectorAll(".qtySection");
-const nameSecs = document.querySelectorAll(".nameSection");
-const detailsTypes = document.querySelectorAll('.typeSectionD');
-
-
-console.log(detailsBtn, detailsBox);
-async function getCableDetails() {
-  try {
-    const response = await fetch(`${BASE_URL}`);
-    const data = await response.json();
-    for (let i = 0; i < data.cables.length; i++) {
-      let itemName = document.createElement("p");
-      let itemQty = document.createElement("p");
-      let itemSize = document.createElement("p");
-      let itemLocation = document.createElement("p");
-      let itemType = document.createElement('p');
-      itemType.innerText = data.cables[i].type;
-      itemSize.innerText = data.cables[i].size;
-      itemLocation.innerText = data.cables[i].location;
-      itemSize.innerText = data.cables[i].size;
-      itemQty.innerText = data.cables[i].quantity;
-      itemName.innerText = data.cables[i].name;
-      itemLocation.style.paddingRight = "17px";
-      itemName.style.paddingRight = "17px";
-      itemSize.style.paddingRight = "17px";
-      itemSize.style.paddingLeft = "17px";
-      itemQty.style.paddingRight = "5px";
-      itemQty.style.paddingLeft = "17px";
-      detailsLocation[0].append(itemLocation);
-      detailsNames[0].append(itemName);
-      detailsQty[0].append(itemQty);
-      detailsSize[0].append(itemSize);
-      detailsTypes[0].append(itemType)
-
-      
-    
-      
-      
-      if (i % 2) {
-        itemLocation.style.color = "rgb(94,94,94)";
-        itemName.style.color = "rgb(94,94,94)";
-        itemQty.style.color = "rgb(94,94,94)";
-        itemSize.style.color = "rgb(94,94,94)";
-        itemType.style.color = "rgb(94,94,94)";
-      } else
-        (itemName.style.backgroundColor = "rgb(59, 120, 204)"),
-          (itemQty.style.backgroundColor = "rgb(59, 120, 204)"),
-          (itemSize.style.backgroundColor = "rgb(59, 120, 204)"),
-          (itemLocation.style.backgroundColor = "rgb(59, 120, 204)"),
-          (itemType.style.backgroundColor = "rgb(59, 120, 204)"),
-          (itemLocation.style.color = "white"),
-          (itemName.style.color = "white"),
-          (itemQty.style.color = "white"),
-          (itemType.style.color = "white"),
-          (itemSize.style.color = "white");
-    }
-    console.log(data.cables[0].type)
-  } catch (e) {
-    console.log("ERROR:", e);
-  }
-}
-
-async function getHardwareDetails() {
-    try {
-      const response = await fetch(`${BASE_URL}`);
-      const data = await response.json();
-      for (let i = 0; i < data.hardware.length; i++) {
-        let itemName = document.createElement("p");
-        let itemQty = document.createElement("p");
-        let itemSize = document.createElement("p");
-        let itemLocation = document.createElement("p");
-        let itemType = document.createElement('p');
-        itemType.innerText = data.hardware[i].type;
-        itemSize.innerText = data.hardware[i].size;
-        itemLocation.innerText = data.hardware[i].location;
-        itemSize.innerText = data.hardware[i].size;
-        itemQty.innerText = data.hardware[i].quantity;
-        itemName.innerText = data.hardware[i].name;
-        itemLocation.style.paddingRight = "17px";
-        itemName.style.paddingRight = "17px";
-        itemSize.style.paddingRight = "17px";
-        itemSize.style.paddingLeft = "17px";
-        itemQty.style.paddingRight = "5px";
-        itemQty.style.paddingLeft = "17px";
-        detailsLocation[2].append(itemLocation);
-        detailsNames[2].append(itemName);
-        detailsQty[2].append(itemQty);
-        detailsSize[2].append(itemSize);
-        detailsTypes[2].append(itemType)
-  
-        
-      
-        
-        
-        if (i % 2) {
-          itemLocation.style.color = "rgb(94,94,94)";
-          itemName.style.color = "rgb(94,94,94)";
-          itemQty.style.color = "rgb(94,94,94)";
-          itemSize.style.color = "rgb(94,94,94)";
-          itemType.style.color = "rgb(94,94,94)";
-        } else
-          (itemName.style.backgroundColor = "rgb(59, 120, 204)"),
-            (itemQty.style.backgroundColor = "rgb(59, 120, 204)"),
-            (itemSize.style.backgroundColor = "rgb(59, 120, 204)"),
-            (itemLocation.style.backgroundColor = "rgb(59, 120, 204)"),
-            (itemType.style.backgroundColor = "rgb(59, 120, 204)"),
-            (itemLocation.style.color = "white"),
-            (itemName.style.color = "white"),
-            (itemQty.style.color = "white"),
-            (itemType.style.color = "white"),
-            (itemSize.style.color = "white");
-      }
-    } catch (e) {
-      console.log("ERROR:", e);
-    }
-  }
-
-
-async function getAccessoryDetails() {
-  try {
-    const response = await fetch(`${BASE_URL}`);
-    const data = await response.json();
-    for (let i = 0; i < data.accessories.length; i++) {
-      let itemName = document.createElement("p");
-      let itemQty = document.createElement("p");
-      let itemSize = document.createElement("p");
-      let itemLocation = document.createElement("p");
-      itemLocation.innerText = data.accessories[i].location;
-      itemSize.innerText = data.accessories[i].size;
-      itemQty.innerText = data.accessories[i].quantity;
-      itemName.innerText = data.accessories[i].name;
-      itemLocation.style.paddingRight = "17px";
-      itemName.style.paddingRight = "17px";
-      itemSize.style.paddingRight = "17px";
-      itemSize.style.paddingLeft = "17px";
-      itemQty.style.paddingRight = "5px";
-      itemQty.style.paddingLeft = "17px";
-      detailsLocation[1].append(itemLocation);
-      detailsNames[1].append(itemName);
-      detailsQty[1].append(itemQty);
-      detailsSize[1].append(itemSize);
-      if (i % 2) {
-        itemLocation.style.color = "rgb(94,94,94)";
-        itemName.style.color = "rgb(94,94,94)";
-        itemQty.style.color = "rgb(94,94,94)";
-        itemSize.style.color = "rgb(94,94,94)";
-      } else
-        (itemName.style.backgroundColor = "rgb(59, 120, 204)"),
-          (itemQty.style.backgroundColor = "rgb(59, 120, 204)"),
-          (itemSize.style.backgroundColor = "rgb(59, 120, 204)"),
-          (itemLocation.style.backgroundColor = "rgb(59, 120, 204)"),
-          (itemLocation.style.color = "white"),
-          (itemName.style.color = "white"),
-          (itemQty.style.color = "white"),
-          (itemSize.style.color = "white");
-    }
-  } catch (e) {
-    console.log("ERROR:", e);
-  }
-}
-
-async function getCables() {
-    try {
-      const response = await fetch(`${BASE_URL}`);
-      const data = await response.json();
-      for (let i = 0; i < data.cables.length; i++) {
-        let itemName = document.createElement("p");
-        let itemQty = document.createElement("p");
-        itemQty.innerText = data.cables[i].quantity;
-        itemName.innerText = data.cables[i].name;
-        nameSecs[0].append(itemName);
-        qtySecs[0].append(itemQty);
-        if (i % 2) {
-          itemName.style.backgroundColor = "none";
-          itemQty.style.backgroundColor = "none";
-        } else
-          (itemName.style.backgroundColor = "grey"),
-            (itemQty.style.backgroundColor = "grey"),
-            (itemName.style.color = "white"),
-            (itemQty.style.color = "white");
-      }
-    } catch (e) {
-      console.log("ERROR:", e);
-    }
-  }
-
-async function getAccessories() {
-  try {
-    const response = await fetch(`${BASE_URL}`);
-    const data = await response.json();
-    for (let i = 0; i < data.accessories.length; i++) {
-      let itemName = document.createElement("p");
-      let itemQty = document.createElement("p");
-      itemQty.innerText = data.accessories[i].quantity;
-      itemName.innerText = data.accessories[i].name;
-      nameSecs[1].append(itemName);
-      qtySecs[1].append(itemQty);
-      if (i % 2) {
-        itemName.style.backgroundColor = "grey";
-        itemQty.style.backgroundColor = "grey";
-        itemName.style.color = "white";
-        itemQty.style.color = "white";
-      }
-    }
-  } catch (e) {
-    console.log("ERROR:", e);
-  }
-}
-
-
-
-detailsBtn.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      detailsBox.forEach((box, idx) => {
-        if (idx === index) {
-          box.classList.toggle("showdetails"); 
-        } else {
-          box.classList.remove("showdetails");
-        }
-      });
-    });
-  });
-
-
-closeDetails.forEach((closeBtn) => {
-    closeBtn.addEventListener('click', ()=> {
-        detailsBox.forEach((box) => {
-            box.classList.remove('showdetails')
-        })
-    })
+flipCard.addEventListener('click', ()=> {
+  flipCard.classList.toggle('flip-cardFlip')
 })
 
 
 
 
 
-async function getHardware() {
+async function getData() {
+  
+
   try {
     const response = await fetch(`${BASE_URL}`);
     const data = await response.json();
-    console.log(data);
-    for (let i = 0; i < data.hardware.length; i++) {
-      let itemName = document.createElement("p");
-      let itemQty = document.createElement("p");
-      itemQty.innerText = data.hardware[i].quantity;
-      itemName.innerText = data.hardware[i].name;
-      nameSecs[2].append(itemName);
-      qtySecs[2].append(itemQty);
-      if (i % 2) {
-        itemName.style.backgroundColor = "grey";
-        itemQty.style.backgroundColor = "grey";
-        itemName.style.color = "white";
-        itemQty.style.color = "white";
-      }
+    // change dataLength
+    const dataLength = data.cables.length;
+
+    for (let i = 0; i < dataLength; i++) {
+      const dataName = data.cables[i].name; // change dataName
+      const dataQty = data.cables[i].quantity;
+      const dataType = data.cables[i].type;
+      const dataLocation = data.cables[i].location;
+      const dataSize = data.cables[i].size;
+      let size = document.createElement('p');
+      let name = document.createElement("p");
+      let location = document.createElement("p");
+      let qty = document.createElement("p");
+      let type = document.createElement("p");
+      let row = document.createElement("div");
+      
+
+     
+      row.classList.add("row");
+      qty.innerText = dataQty;
+      location.innerText = dataLocation;
+      type.innerText = dataType;
+      name.innerText = dataName;
+      size.innerText = dataSize
+      right[0].append(row);
+      row.append( name, qty, size, type,  location);
+      console.log(data.cables[i]);
     }
   } catch (e) {
-    console.log("ERROR:", e);
+    console.log("ERROR", e);
   }
 }
 
+// CloseBTns
+closeBtn[0].addEventListener('click',()=> {
+  right[0].classList.toggle('remove')
+})
+closeBtn[1].addEventListener('click',()=> {
+  right[1].classList.toggle('remove')
+})
+closeBtn[2].addEventListener('click',()=> {
+  right[2].classList.toggle('remove')
+})
+// 
+
+
+  const items = document.querySelectorAll('.item');
+items[0].addEventListener('click', () => {
+ right[0].classList.toggle('remove')
+})
+items[1].addEventListener('click', () => {
+  right[1].classList.toggle('remove')
+ })
+ items[2].addEventListener('click', () => {
+  right[2].classList.toggle('remove')
+ })
+ items[3].addEventListener('click', () => {
+  flipper.classList.toggle('flipshower')
+})
+items[4].addEventListener('click', () => {
+  whoops.classList.toggle('show')
+})
+ 
+
+async function getData1() {
+  
+
+  try {
+    const response = await fetch(`${BASE_URL}`);
+    const data = await response.json();
+    // change dataLength
+    const dataLength = data.hardware.length;
+
+    for (let i = 0; i < dataLength; i++) {
+      const dataName = data.hardware[i].name; // change dataName
+      const dataQty = data.hardware[i].quantity;
+      const dataType = data.hardware[i].type;
+      const dataLocation = data.hardware[i].location;
+      const dataSize = data.hardware[i].size;
+      let size = document.createElement('p');
+      let name = document.createElement("p");
+      let location = document.createElement("p");
+      let qty = document.createElement("p");
+      let type = document.createElement("p");
+      let row = document.createElement("div");
+      
+
+     
+      row.classList.add("row");
+      qty.innerText = dataQty;
+      location.innerText = dataLocation;
+      type.innerText = dataType;
+      name.innerText = dataName;
+      size.innerText = dataSize
+      right[1].append(row);
+      row.append( name, qty, size, type,  location);
+    }
+  } catch (e) {
+    console.log("ERROR", e);
+  }
+}
+
+async function getData2() {
+  
+
+  try {
+    const response = await fetch(`${BASE_URL}`);
+    const data = await response.json();
+    // change dataLength
+    const dataLength = data.accessories.length;
+
+    for (let i = 0; i < dataLength; i++) {
+      const dataName = data.accessories[i].name; // change dataName
+      const dataQty = data.accessories[i].quantity;
+      const dataType = data.accessories[i].type;
+      const dataLocation = data.accessories[i].location;
+      const dataSize = data.accessories[i].size;
+      let size = document.createElement('p');
+      let name = document.createElement("p");
+      let location = document.createElement("p");
+      let qty = document.createElement("p");
+      let type = document.createElement("p");
+      let row = document.createElement("div");
+      
+
+     
+      row.classList.add("row");
+      qty.innerText = dataQty;
+      location.innerText = dataLocation;
+      type.innerText = dataType;
+      name.innerText = dataName;
+      size.innerText = dataSize
+      right[2].append(row);
+      row.append( name, qty, size, type,  location);
+    }
+  } catch (e) {
+    console.log("ERROR", e);
+  }
+}
+
+getData2()
+getData1()
+getData()
 
 
 
-getHardwareDetails();
-getAccessoryDetails();
-getCableDetails();
-export { getCables, getHardware, getAccessories };
+
